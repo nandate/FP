3feb02d7ef9515d220bdc96455a6238d15ea86f0
nandate <o238049t@mail.kyutech.jp>
2018-01-30 18:51:24 +0900

    rename reservations_controller to tickets_controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,4 @@
-class ReservationsController < ApplicationController
+class TicketsController < ApplicationController
   before_action :authenticate_user!
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "visitors#index"
   resources :users
   resources :timesheets
-  resource :reservation
+  resources :tickets
 end
