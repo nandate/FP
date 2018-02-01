36fc3a92bace3ce0a399c7c90b1757c4d417ee75
nandate <o238049t@mail.kyutech.jp>
2018-02-01 13:23:55 +0900

    add reservation approval function

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,12 +8,23 @@ class TicketsController < ApplicationController
       user: current_user, timesheet: timesheet)
     workflow.run
     if workflow.success
+      flash[:success] = "予約しました。"
       redirect_to current_user
     else
       redirect_to timesheets_path
     end
   end
 
-
+  def update
+    timesheet = Timesheet.find(params[:timesheet_id])
+    workflow = ApproveReservation.new(timesheet: timesheet)
+    workflow.run
+    if workflow.success
+      flash[:success] = "予約を承認しました。"
+      redirect_to current_user
+    else
+      redirect_to root_path
+    end
+  end
 
 end
---
+++ app/workflows/approve_reservation.rb
@@ -0,0 +1,19 @@
+class ApproveReservation
+  attr_accessor :timesheet, :success
+
+  def initialize(user:, timesheet: )
+    @timesheet = timesheet
+    @success = false
+  end
+
+  def run
+    Ticket.transaction do
+      ticket = timesheet.ticket
+      return unless ticket.waiting?
+      ticket.update(status: :approved)
+      self.success = ticket.valid?
+      success
+    end
+  end
+
+end
