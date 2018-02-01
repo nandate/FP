5c533087307c2c2841ff20f8c6b44e1a8fe14585
nandate <o238049t@mail.kyutech.jp>
2018-02-01 19:20:54 +0900

    承認待ちのイベント取得時のN+1問題の対処

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :tickets
 
   def waiting_tickets
-    tickets.waiting.all.to_a
+    tickets.waiting.includes(:timesheet)
   end
 
   def approved_tickets
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   end
 
   def waiting_reservations
-    waiting_tickets.map(&:timesheet).uniq.sort_by(&:start_time)
+    waiting_tickets.map(&:timesheet)
   end
 
   def approved_reservations
