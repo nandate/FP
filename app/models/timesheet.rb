5e3e7d607631df671703e2bc09043ceb949631d1
nandate <o238049t@mail.kyutech.jp>
2018-02-01 16:47:19 +0900

    private化

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -5,23 +5,24 @@ class Timesheet < ApplicationRecord
   validate :validate_past, if: :past_time?
   validate :validate_start_time
 
-  def past_time?
-    start_time < Time.now
-  end
-
-  def validate_past
-    errors.add(:start_time, ":過去の日付は使用できません。")
-  end
-
-  def validate_start_time
-    case start_time.wday
-    when 0
-      errors.add(:start_time,":日曜日は作成できません。")
-    when 6
-      errors.add(:start_time, ":土曜日は11:00~15:00のみ作成できます。") if start_time.hour < 11 || start_time.hour > 15
-    else
-      errors.add(:start_time, "平日は10:00~18:00のみ作成できます。") if start_time.hour < 10 || start_time.hour > 18
+  private
+    def past_time?
+      start_time < Time.now
     end
-  end
+
+    def validate_past
+      errors.add(:start_time, ":過去の日付は使用できません。")
+    end
+
+    def validate_start_time
+      case start_time.wday
+      when 0
+        errors.add(:start_time,":日曜日は作成できません。")
+      when 6
+        errors.add(:start_time, ":土曜日は11:00~15:00のみ作成できます。") if start_time.hour < 11 || start_time.hour > 15
+      else
+        errors.add(:start_time, "平日は10:00~18:00のみ作成できます。") if start_time.hour < 10 || start_time.hour > 18
+      end
+    end
 
 end
