8fe226836ff2b05c042eecbaf93f7445ef8f0ddd
nandate <o238049t@mail.kyutech.jp>
2018-02-06 17:07:13 +0900

    usersテーブルからcurrent_sign_in_ipカラムとlast_sign_in_ipカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125054422) do
+ActiveRecord::Schema.define(version: 20180206080406) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 20180125054422) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
