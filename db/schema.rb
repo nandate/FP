cb37b1b8cfe57f678f853370f4010b0e05124ba9
nandate <o238049t@mail.kyutech.jp>
2018-02-09 14:25:09 +0900

    Tickestテーブルのstatusカラムにnull: falseの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207090846) do
+ActiveRecord::Schema.define(version: 20180209052337) do
 
   create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
     t.bigint "timesheet_id"
-    t.integer "status"
+    t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["timesheet_id"], name: "index_tickets_on_timesheet_id"
