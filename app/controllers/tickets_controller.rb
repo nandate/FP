67cbaacb88dbe2a27b1c96a65d62b400c66f1055
nandate <o238049t@mail.kyutech.jp>
2018-02-09 13:41:17 +0900

    timesheetの読み込み部分の切り出し

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,11 @@
 class TicketsController < ApplicationController
   before_action :authenticate_user!
+  before_action :load_timesheet, only: [:create, :update]
 
   def create
-    timesheet = Timesheet.find(params[:timesheet_id])
-    return unless timesheet.ticket.nil?
+    return unless @timesheet.ticket.nil?
     workflow = CreateReservation.new(
-      user: current_user, timesheet: timesheet)
+      user: current_user, timesheet: @timesheet)
     workflow.run
     if workflow.success
       redirect_to current_user
@@ -13,5 +13,21 @@ class TicketsController < ApplicationController
       redirect_to timesheets_path
     end
   end
+
+  def update
+    workflow = ApproveReservation.new(timesheet: timesheet)
+    workflow.run
+    if workflow.success
+      flash[:success] = "予約を承認しました。"
+      redirect_to current_user
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+    def load_timesheet
+      @timesheet = Timesheet.find(params[:timesheet_id])
+    end
 
 end
