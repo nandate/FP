b912a2d6d9563a32d6459613e8997322fca5924f
nandate <o238049t@mail.kyutech.jp>
2018-02-09 14:13:24 +0900

    Ticketモデルのuserとtimesheetが空でないかの検証

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :timesheet
+  validates :user, presence: true
+  validates :timesheet, presence: true
 
   enum status: { waiting: 0 , approved: 1}
 end
