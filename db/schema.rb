7fceb72ab4c3de70e514c4b9365ee0155981bdc4
nandate <o238049t@mail.kyutech.jp>
2018-02-20 22:35:44 +0900

    Ticketのuser_idとtimesheet_idの組み合わせにindexを貼った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209055001) do
+ActiveRecord::Schema.define(version: 20180220132200) do
 
   create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180209055001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["timesheet_id"], name: "index_tickets_on_timesheet_id"
+    t.index ["user_id", "timesheet_id"], name: "index_tickets_on_user_id_and_timesheet_id", unique: true
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
