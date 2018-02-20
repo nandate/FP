4490bc1f1e610b032cfa0fd17c5e7f7a26a8386f
nandate <o238049t@mail.kyutech.jp>
2018-02-20 22:20:27 +0900

    nameカラムにindexを貼った

--- db/migrate/20180209055001_add_not_null_and_unique_index_to_user.rb
+++ db/migrate/20180209055001_add_not_null_and_unique_index_to_user.rb
@@ -1,6 +1,6 @@
 class AddNotNullAndUniqueIndexToUser < ActiveRecord::Migration[5.1]
   def change
     change_column_null :users, :name, false
-    add_index :users, [:name, :id], unique: true
+    add_index :users, [:name], unique: true
   end
 end
