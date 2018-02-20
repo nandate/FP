091e437e667ce8996acef5bccb1d1a8cff4e039b
nandate <o238049t@mail.kyutech.jp>
2018-02-20 22:20:27 +0900

    nameカラムにindexを貼った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209052337) do
+ActiveRecord::Schema.define(version: 20180209055001) do
 
   create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -42,8 +42,9 @@ ActiveRecord::Schema.define(version: 20180209052337) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
