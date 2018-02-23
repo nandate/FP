e2fa682bde442e4b154da6713b8501cc45b9b7b1
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-02-23 16:01:59 +0900

    ticket作成の例外処理

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,18 +3,17 @@ class TicketsController < ApplicationController
   before_action :load_timesheet, only: [:create, :update]
 
   def create
-    return unless @timesheet.ticket.nil?
+    #return unless @timesheet.ticket.nil?
     create_reservation = CreateReservation.new(
       user: current_user,
       timesheet: @timesheet
     )
-    if create_reservation.run
-      flash[:success] = "正常に予約できました。"
-      redirect_to current_user
-    else
-      flash[:danger] = "正常に予約ができませんでした。"
-      redirect_to timesheets_path
-    end
+    create_reservation.run!
+    flash[:success] = "予約に成功しました。"
+    redirect_to current_user
+  rescue ActiveRecord::RecordInvalid => e
+    flash[:danger] = "予約に失敗しました。#{e.record.errors}"
+    redirect_to timesheets_path
   end
 
   def update
