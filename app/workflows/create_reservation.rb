bde75fd59e510f5e935b6bbd0da2cece01981f87
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-02-28 16:18:20 +0900

    Ticket作成の部分の修正

--- app/workflows/create_reservation.rb
+++ app/workflows/create_reservation.rb
@@ -1,15 +1,14 @@
 class CreateReservation
   attr_reader :user, :timesheet
 
-  def initialize(user:, timesheet: )
+  def initialize(user:, timesheet:)
     @user = user
     @timesheet = timesheet
   end
 
   def run!
     Ticket.transaction do
-      user.tickets.create!(user: user, timesheet: timesheet, status: "waiting")
+      user.tickets.create!(timesheet: timesheet, status: "waiting")
     end
   end
-
 end
