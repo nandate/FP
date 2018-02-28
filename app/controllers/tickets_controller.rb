489a913ee1ed3a0f64ba768a1a6c392b625866d2
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-02-28 14:04:45 +0900

    rubocopの指摘の修正とリダイレクトとflashを一行にまとめた

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,47 +1,41 @@
 class TicketsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_timesheet, only: [:create, :update]
-  before_action :correct_user, only: [:destroy]
+  before_action :load_timesheet, only: %i(create update)
+  before_action :correct_user, only: %i(destroy)
 
   def create
-    #return unless @timesheet.ticket.nil?
     create_reservation = CreateReservation.new(
       user: current_user,
       timesheet: @timesheet
     )
     create_reservation.run!
-    flash[:success] = "予約に成功しました。"
-    redirect_to current_user
+    redirect_to current_user, success: "予約に成功しました。"
   rescue ActiveRecord::RecordInvalid => e
-    flash[:danger] = "予約に失敗しました。#{e.record.errors}"
-    redirect_to timesheets_path
+    redirect_to timesheets_path, danger: "予約に失敗しました。#{e.record.errors}"
   end
 
   def update
     approve_reservation = ApproveReservation.new(timesheet: @timesheet)
     if approve_reservation.run
-      flash[:success] = "予約を承認しました。"
-      redirect_to current_user
+      redirect_to current_user, success: "予約を承認しました。"
     else
-      flash[:danger] = "予約の承認に失敗しました。"
-      redirect_to current_user
+      redirect_to current_user, danger: "予約の承認に失敗しました。"
     end
   end
 
   def destroy
     @ticket.destroy
-    flash[:success] = "予約をキャンセルしました。"
-    redirect_to current_user
+    redirect_to current_user, success: "予約をキャンセルしました。"
   end
 
   private
-    def load_timesheet
-      @timesheet = Timesheet.find(params[:timesheet_id])
-    end
 
-    def correct_user
-      @ticket = current_user.tickets.find(params[:id])
-      redirect_to root_url if @ticket.nil?
-    end
+  def load_timesheet
+    @timesheet = Timesheet.find(params[:timesheet_id])
+  end
 
+  def correct_user
+    @ticket = current_user.tickets.find(params[:id])
+    redirect_to root_url if @ticket.nil?
+  end
 end
