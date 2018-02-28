051f36c1212c4eefe98090f5ff3b5d8eb21df92d
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-02-28 18:07:15 +0900

    rubocopの指摘の修正

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -10,34 +10,34 @@ class Timesheet < ApplicationRecord
   scope :approved, -> { joins(:ticket).merge(Ticket.approved) }
 
   private
-    def past_time?
-      start_time < Time.zone.now
-    end
 
-    def validate_past
-      errors.add(:start_time, ":過去の日付は使用できません。")
-    end
+  def past_time?
+    start_time < Time.zone.now
+  end
 
-    def validate_double_book
-      timesheet = Timesheet.where(user_id: user_id).where(start_time: start_time)
-      if timesheet.any?
-        errors.add(:start_time, ":この時間にはすでに予約が作成されています。")
+  def validate_past
+    errors.add(:start_time, ":過去の日付は使用できません。")
+  end
+
+  def validate_double_book
+    timesheet = Timesheet.where(user_id: user_id).where(start_time: start_time)
+    if timesheet.any?
+      errors.add(:start_time, ":この時間にはすでに予約が作成されています。")
+    end
+  end
+
+  def validate_start_time
+    case start_time.wday
+    when 0
+      errors.add(:start_time, ":日曜日は作成できません。")
+    when 6
+      if start_time.hour < 11 || start_time.hour > 15
+        errors.add(:start_time, ":土曜日は11:00~15:00のみ作成できます。")
+      end
+    else
+      if start_time.hour < 10 || start_time.hour > 18
+        errors.add(:start_time, "平日は10:00~18:00のみ作成できます。")
       end
     end
-
-    def validate_start_time
-      case start_time.wday
-      when 0
-        errors.add(:start_time, ":日曜日は作成できません。")
-      when 6
-        if start_time.hour < 11 || start_time.hour > 15
-          errors.add(:start_time, ":土曜日は11:00~15:00のみ作成できます。")
-        end
-      else
-        if start_time.hour < 10 || start_time.hour > 18
-          errors.add(:start_time, "平日は10:00~18:00のみ作成できます。")
-        end
-      end
-    end
-
+  end
 end
