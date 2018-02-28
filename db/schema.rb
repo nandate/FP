fc2e8f2e3e96ed1c08499a7b8dab9aee8f5ace57
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-02-28 13:34:51 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20180220132200) do
     t.integer "role"
     t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["name", "id"], name: "index_users_on_name_and_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
