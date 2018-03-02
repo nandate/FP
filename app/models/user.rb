1af15165622abc0e95759848a95f274bef9d287a
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-02 18:55:44 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: {user: 0, fp: 1}
+  enum role: { user: 0, fp: 1 }
   has_many :timesheets
   has_many :tickets
+  has_many :approved_tickets
   has_many :applied_timesheets, through: :tickets, source: 'timesheet'
 
   def waiting_reservations
@@ -16,5 +17,4 @@ class User < ApplicationRecord
   def approved_reservations
     applied_timesheets.approved
   end
-
 end
