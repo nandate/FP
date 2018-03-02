b40a683337c2a222f92cfd4304f202f5244b53ba
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-02 19:05:31 +0900

    予約の承認(approved_ticketのcreate)

--- app/controllers/approved_tickets_controller.rb
+++ app/controllers/approved_tickets_controller.rb
@@ -1,7 +1,16 @@
 class ApprovedTicketsController < ApplicationController
+  before_action :load_timesheet
   def create
+    current_user.approved_tickets.create!(timesheet: @timesheet)
+    redirect_to current_user, success: "予約を承認しました。"
   end
 
   def destroy
+  end
+
+  private
+
+  def load_timesheet
+    @timesheet = current_user.timesheets.find(params[:timesheet_id])
   end
 end
