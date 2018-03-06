8531dcc772fbf9f2000b8acc8bbbaa02fd39d419
nandate <o238049t@mail.kyutech.jp>
2018-03-06 21:44:33 +0900

    user#show部分のN+1対策(normal_user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @timesheets = @user.timesheets.includes(:tickets)
+    @tickets = @user.tickets.includes(:timesheet)
   end
 end
