e0da8a3bb8b94310be820de99cb886b9bef89a16
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-08 18:14:13 +0900

    バリデーションメソッドの削除

--- app/models/approved_ticket.rb
+++ app/models/approved_ticket.rb
@@ -2,13 +2,5 @@ class ApprovedTicket < ApplicationRecord
   belongs_to :timesheet
   belongs_to :ticket
   validates :timesheet, presence: true
-  validate :validate_duplicate_approve
-
-  private
-
-  def validate_duplicate_approve
-    if user.approved_timesheets.where(start_time: timesheet.start_time)
-      errors.add(:base, "この時間にはすでに承認した予約があります。")
-    end
-  end
+  validates :ticket, presence: true
 end
