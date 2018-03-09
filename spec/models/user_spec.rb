bd55aef8f0704f60ef77778cefb4726c7e813ef1
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-09 15:10:07 +0900

    userモデルのバリデーションのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, email, and password" do
+    user = User.new(
+      name: "taro",
+      email: "aaa@example.com",
+      password: "password"
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with duplicate email address" do
+    User.create(
+      name: "joe",
+      email: "tester@example.com",
+      password: "password"
+    )
+    user = User.new(
+      name: "jane",
+      email: "tester@example.com",
+      password: "password"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 end
