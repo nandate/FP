c528208e2d8057b9a6eadea8b0ab6579b960440b
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-09 10:06:11 +0900

    approved_ticketのtimesheet, ticketにuniquenessのバリデーションを追加

--- app/models/approved_ticket.rb
+++ app/models/approved_ticket.rb
@@ -1,6 +1,6 @@
 class ApprovedTicket < ApplicationRecord
   belongs_to :timesheet
   belongs_to :ticket
-  validates :timesheet, presence: true
-  validates :ticket, presence: true
+  validates :timesheet, presence: true, uniqueness: true
+  validates :ticket, presence: true, uniqueness: true
 end
