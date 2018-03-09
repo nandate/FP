47f211e35df0fab704e711dbbef4bd693b73d475
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-09 17:22:10 +0900

    ログイン後のusers#showのテスト

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #show" do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    it "can get it" do
+      sign_in @user
+      get :show, params: { id: @user.id }
+      expect(response.status).to eq 200
+    end
+  end
 end
