9a0849ca940a44da08478fd0f2cba1c816185d2f
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-09 15:12:40 +0900

    userモデルのnameカラムに空でないことのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :timesheets
   has_many :tickets
   has_many :applied_timesheets, through: :tickets, source: 'timesheet'
+  validates :name, presence: true
 end
