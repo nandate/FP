8dfe4e5c102187e50295b7027a85e470acda637d
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-09 18:59:22 +0900

    users#showをletを使って書き直す

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
-    before do
-      @user = FactoryBot.create(:user)
-    end
+    let(:user) { FactoryBot.create(:user) }
 
     it "can get it" do
-      sign_in @user
-      get :show, params: { id: @user.id }
+      sign_in user
+      get :show, params: { id: user.id }
+      expect(response).to be_success
       expect(response.status).to eq 200
     end
   end
