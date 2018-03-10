4d27f94ff20f99713e02dab20a20982dba37a062
nandate <o238049t@mail.kyutech.jp>
2018-03-10 21:41:33 +0900

    Revert "Fix validate double book"

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,14 +3,6 @@ class Ticket < ApplicationRecord
   belongs_to :timesheet
   validates :user, presence: true
   validates :timesheet, presence: true
-  validate :validate_duplicate_request
+
   enum status: { waiting: 0, approved: 1 }
-
-  private
-
-  def validate_duplicate_request
-    if user.applied_timesheets.where(start_time: timesheet.start_time).any?
-      errors.add(:base, "すでに予約申請中です。")
-    end
-  end
 end
