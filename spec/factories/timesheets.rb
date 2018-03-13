11cd81588f861162a91bc67c9c8a9c851cb9ef50
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 17:01:21 +0900

    timesheetが有効かどうかのテストの追加

--- spec/factories/timesheets.rb
+++ spec/factories/timesheets.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :timesheet do
-    start_time Time.zone.local(2019, 12, 6, 0o0)
+    start_time Time.zone.local(2019, 12, 6, 12, 0, 0)
     association :user
   end
 end
--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Timesheet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:timesheet) { build(:timesheet) }
+
+  it { expect(timesheet).to be_valid }
 end
