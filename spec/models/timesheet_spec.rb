0f01413c2d49977fdf0aead43c2d8878f59377fa
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 17:54:23 +0900

    時間が30分単位かどうかのテストの追加

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe Timesheet, type: :model do
       expect(timesheet).not_to be_valid
       expect(timesheet.errors[:start_time]).to include(':過去の日付は使用できません。')
     end
+
+    it 'is invalid with a time width is not 30 minutes' do
+      timesheet = build(:timesheet, start_time: Time.zone.local(2019, 12, 6, 12, 13, 0))
+      expect(timesheet).not_to be_valid
+      expect(timesheet.errors[:start_time]).to include(':この時刻では作成できません。')
+    end
   end
 end
