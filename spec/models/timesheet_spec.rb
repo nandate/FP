44d78506e1d4e49f3856b0f1ef681a99eb0371d2
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 17:03:43 +0900

    userとのアソシエーションのテストの追加

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Timesheet, type: :model do
   let(:timesheet) { build(:timesheet) }
 
   it { expect(timesheet).to be_valid }
+
+  describe 'association test' do
+    it { is_expected.to belong_to(:user) }
+  end
 end
