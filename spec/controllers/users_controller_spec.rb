8602f74e099589bd39cddf9b2f40e5f3b38ade2f
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 12:32:15 +0900

    ログインしている場合とそうでない場合を分けた

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,11 +4,24 @@ RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
     let(:user) { create(:user) }
 
-    it "can get it" do
-      sign_in user
-      get :show, params: { id: user.id }
-      expect(response).to be_success
-      expect(response.status).to eq 200
+    context "as logged in" do
+      before do
+        sign_in user
+      end
+
+      it "responds successfully" do
+        get :show, params: { id: user.id }
+        expect(response).to be_success
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "as not logged in" do
+      it "returns a 302 response" do
+        get :show, params: { id: user.id }
+        expect(response).to redirect_to "/users/sign_in"
+        expect(response.status).to eq 302
+      end
     end
   end
 end
