f53b9037a29803aeca211be2cf692e6dc6d166eb
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 17:41:40 +0900

    過去の時間の検証のテスト

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Timesheet, type: :model do
   describe 'association test' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe 'validation test' do
+    it 'is invalid with a past time' do
+      timesheet = build(:timesheet, start_time: Time.zone.local(2000))
+      expect(timesheet).not_to be_valid
+      expect(timesheet.errors[:start_time]).to include(':過去の日付は使用できません。')
+    end
+  end
 end
