5d8e7fd5c58a3090186ffa4aff1593987d19df27
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 15:40:14 +0900

    認可されているかどうかの切り出し

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,31 +8,33 @@ RSpec.describe UsersController, type: :controller do
     context "as logged in" do
       before do
         sign_in user
-        get :show, params: { id: user.id }
       end
 
-      it "responds successfully" do
-        expect(response).to be_success
-        expect(response.status).to eq 200
+      context "as an authorized user" do
+        before do
+          get :show, params: { id: user.id }
+        end
+        it "responds successfully" do
+          expect(response).to be_success
+          expect(response.status).to eq 200
+        end
+
+        it "assigns the requested user to @user" do
+          expect(assigns(:user)).to eq user
+        end
       end
 
-      it "assigns the requested user to @user" do
-        expect(assigns(:user)).to eq user
+      context "as an unauthorized user" do
+        before do
+          get :show, params: { id: user2.id }
+        end
+
+        it "redirect_to root_url" do
+          expect(response).to redirect_to root_url
+          expect(response.status).to eq 302
+        end
       end
     end
-
-    context "as an unauthorized user" do
-      before do
-        sign_in user
-        get :show, params: { id: user2.id }
-      end
-
-      it "redirect_to root_url" do
-        expect(response).to redirect_to root_url
-        expect(response.status).to eq 302
-      end
-    end
-
     context "as not logged in" do
       it "returns a 302 response" do
         get :show, params: { id: user.id }
