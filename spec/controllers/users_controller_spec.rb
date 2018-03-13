ccddacd829054044f2ae95c939d2857a934119ab
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-13 15:17:57 +0900

    自分以外のuserを見ようとしたらrootにリダイレクトされるかどうかのテスト

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
     let(:user) { create(:user) }
+    let(:user2) { create(:user, name: "taro", email: "test2@example.com") }
 
     context "as logged in" do
       before do
@@ -17,6 +18,18 @@ RSpec.describe UsersController, type: :controller do
 
       it "assigns the requested user to @user" do
         expect(assigns(:user)).to eq user
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        sign_in user
+        get :show, params: { id: user2.id }
+      end
+
+      it "redirect_to root_url" do
+        expect(response).to redirect_to root_url
+        expect(response.status).to eq 302
       end
     end
 
