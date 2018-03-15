266728fe7f66880e33d1fb12e20efdd1741d1b21
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-15 18:21:34 +0900

    POST #createの記述(ルーティングエラー)

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  let(:user) { create(:user, name: "a", email: "a@example.com") }
+  let(:timesheet) { create(:timesheet) }
 
+  describe 'POST #create' do
+    before { sign_in user }
+
+    let(:params) do
+      {
+        ticket: {
+          user_id: user.id,
+          timesheet_id: timesheet.id
+        }
+      }
+    end
+    subject { post :create, params: params }
+
+    context 'create success' do
+      it { expect(subject.status).to eq 302 }
+      it { expect { subject }.to change(Ticket, :count).by(1) }
+      it { expect(subject).to redirect_to current_user }
+    end
+  end
 end
