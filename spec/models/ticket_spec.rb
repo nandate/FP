9e06812765b0dcdc26bf84c8adac3686ede850bb
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-15 11:41:33 +0900

    ticketか有効かどうかの検証

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:fp_user) { create(:fp_user) }
+  let(:timesheet) { create(:timesheet, user: fp_user) }
+  let(:ticket) { build(:ticket, user: user, timesheet: timesheet) }
+
+  it { expect(ticket).to be_valid }
 end
