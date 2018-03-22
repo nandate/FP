84d84977712eda2c1d97d5521544d826475df371
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-22 11:23:19 +0900

    nameとemailの長さの検証のテストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:role) }
     it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_length_of(:email).is_at_most(255) }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
     it { is_expected.to validate_length_of(:password).is_at_most(128) }
   end
