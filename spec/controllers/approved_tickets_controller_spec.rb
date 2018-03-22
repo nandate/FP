6cab7dfb8b83ea9ff8a5fbc790a82a18feab8619
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-22 16:33:13 +0900

    他のfpがcreateしようとした場合のテストの追加

--- spec/controllers/approved_tickets_controller_spec.rb
+++ spec/controllers/approved_tickets_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ApprovedTicketsController, type: :controller do
   let(:fp_user) { create(:fp_user) }
+  let(:other_fp_user) { create(:other_fp_user) }
   let(:timesheet) { create(:timesheet, user: fp_user) }
   let(:ticket) { create(:ticket, timesheet: timesheet) }
 
   describe 'POST #create' do
-    before { sign_in fp_user }
     let(:params) do
       {
         timesheet_id: timesheet.id,
@@ -19,10 +19,24 @@ RSpec.describe ApprovedTicketsController, type: :controller do
     end
     subject { post :create, params: params }
 
-    context 'create success' do
-      it { expect(subject.status).to eq 302 }
-      it { expect(subject).to redirect_to fp_user }
-      it { expect { subject }.to change(ApprovedTicket, :count).by(1) }
+    context 'an authorized user' do
+      before { sign_in fp_user }
+
+      context 'create success' do
+        it { expect(subject.status).to eq 302 }
+        it { expect(subject).to redirect_to fp_user }
+        it { expect { subject }.to change(ApprovedTicket, :count).by(1) }
+      end
+    end
+
+    context 'an unauthorized user ' do
+      before { sign_in other_fp_user }
+
+      context 'create failed' do
+        it { expect { subject }.not_to change(ApprovedTicket, :count) }
+        it { expect(subject).to redirect_to root_url }
+        it { expect(subject.status).to eq 302 }
+      end
     end
   end
 end
