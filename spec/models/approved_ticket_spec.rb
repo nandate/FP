5a77c9ea55d83b2add051a92c7a081a95ec49d72
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-22 15:39:39 +0900

    approved_ticketが有効かどうかのテストの追加

--- spec/models/approved_ticket_spec.rb
+++ spec/models/approved_ticket_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ApprovedTicket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:approved_ticket) { build(:approved_tickets) }
+
+  it { expect(approved_ticket).to be_valid }
 end
