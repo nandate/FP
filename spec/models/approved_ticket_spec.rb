354f94a885efe9cdb30d0284e1d404c595984a32
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-22 15:40:53 +0900

    timesheetとticketが空でないかのテストの追加

--- spec/models/approved_ticket_spec.rb
+++ spec/models/approved_ticket_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe ApprovedTicket, type: :model do
   let(:approved_ticket) { build(:approved_tickets) }
 
   it { expect(approved_ticket).to be_valid }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:timesheet) }
+    it { is_expected.to validate_presence_of(:ticket) }
+  end
 end
