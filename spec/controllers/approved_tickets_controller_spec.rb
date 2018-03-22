3e0ffb0915e3c89133f67a7a65b808a598e41780
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-22 16:16:29 +0900

    POST #createのテストの追加

--- spec/controllers/approved_tickets_controller_spec.rb
+++ spec/controllers/approved_tickets_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ApprovedTicketsController, type: :controller do
+  let(:fp_user) { create(:fp_user) }
+  let(:timesheet) { create(:timesheet, user: fp_user) }
+  let(:ticket) { create(:ticket, timesheet: timesheet) }
 
+  describe 'POST #create' do
+    before { sign_in fp_user }
+    let(:params) do
+      {
+        timesheet_id: timesheet.id,
+        ticket_id: ticket.id,
+        approved_ticket: {
+          timesheet_id: timesheet.id,
+          ticket_id: ticket.id
+        }
+      }
+    end
+    subject { post :create, params: params }
+
+    context 'create success' do
+      it { expect(subject.status).to eq 302 }
+      it { expect(subject).to redirect_to fp_user }
+      it { expect { subject }.to change(ApprovedTicket, :count).by(1) }
+    end
+  end
 end
