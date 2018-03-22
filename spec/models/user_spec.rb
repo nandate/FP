6e6f94983881d17d24a842e47833cf4f399d8b1c
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-03-22 10:23:31 +0900

    validation等をdescribeでまとめた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
   it { expect(user).to be_valid }
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_presence_of(:role) }
-  it { is_expected.to validate_presence_of(:password) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:role) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 
   describe "is invalid with a duplicate email address" do
     subject { build(:user) }
